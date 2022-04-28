36f05d210d99c7ce25d0d098ac3c5bc7f7b93626
Michel Thomas Brodatzki <michel@brodatzki.com>
2022-04-28 19:56:13 +0200

    Refine SpecApi Swagger doc and tags

--- getraenke/endpoints/spec.py
+++ getraenke/endpoints/spec.py
@@ -9,16 +9,24 @@ class SpecApi(MethodView):
     def get(self):
         """
         Returns Swagger specification for this project
+        This project is documented using docstrings. These docstrings can be parsed using flask_swagger, whose output will be returned by this endpoint in a json-ified form.
         ---
         tags:
             - documentation
-        respsonses:
+        responses:
             200:
-                description: Returns jsonified specification
+                description: Returns jsonified Swagger specification
         """
 
         swag = swagger(app)
         swag['info']['version'] = "1.0"
         swag['info']['title'] = "API des digitalen Getraenkeverkaufs"
+
+        swag['tags'] = [
+            {
+                'name': 'documentation',
+                'description': 'Swagger specification endpoint.'
+            }
+        ];
 
         return jsonify(swag)
