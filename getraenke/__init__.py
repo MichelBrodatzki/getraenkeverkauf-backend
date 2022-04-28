d09c6d842053e8abc4bc6da462929422e80f3871
Michel Thomas Brodatzki <michel@brodatzki.com>
2022-04-28 19:55:35 +0200

    Disable CORS restrictions

--- getraenke/__init__.py
+++ getraenke/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from getraenke.endpoints.spec import SpecApi
 
 def create_app(config=None):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
 
     app.add_url_rule("/spec", view_func=SpecApi.as_view("spec_api"))
 
