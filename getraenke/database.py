6c20c4f9c071cf214f62a6b08805a17515af83aa
Michel Thomas Brodatzki <michel@brodatzki.com>
2022-04-28 23:27:04 +0200

    Add database object manager

---
+++ getraenke/database.py
@@ -0,0 +1,20 @@
+from flask import g
+from os import getenv
+
+from playhouse.db_url import connect
+
+def get_database():
+    """
+    Returns existing database object, if it exists, else it connects to database and returns new database object.
+    """
+    if 'database' not in g:
+        # Open new database connection, if it doesn't exist yet
+        database_url = getenv("DATABASE_URL", None)
+
+        if database_url is None:
+            print ("No database url set! Defaulting to temporary in-memory database.")
+
+        g.database = connect(database_url or 'sqlite:///:memory:')
+
+    # Return existing database connection
+    return g.database
